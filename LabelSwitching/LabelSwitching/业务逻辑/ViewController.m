4a5a774fb5c44a7be1cac6e71a1c2a0258546eda
Jobs <lg295060456@vip.qq.com>
2021-11-19 21:08:40 +0700

    .
    Signed-off-by: Jobs <lg295060456@vip.qq.com>

---
+++ LabelSwitching/LabelSwitching/业务逻辑/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  LabelSwitching
+//
+//  Created by Jobs on 2021/11/19.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
---
+++ LabelSwitching/LabelSwitchingTests/LabelSwitchingTests.m
@@ -0,0 +1,36 @@
+//
+//  LabelSwitchingTests.m
+//  LabelSwitchingTests
+//
+//  Created by Jobs on 2021/11/19.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface LabelSwitchingTests : XCTestCase
+
+@end
+
+@implementation LabelSwitchingTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
