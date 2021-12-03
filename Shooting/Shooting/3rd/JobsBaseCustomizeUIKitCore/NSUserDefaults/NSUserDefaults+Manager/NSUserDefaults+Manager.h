b9678439e69df8fb5157ad750f0285a72ff27c61
Jobs <lg295060456@vip.qq.com>
2021-12-03 20:06:55 +0700

    .
    Signed-off-by: Jobs <lg295060456@vip.qq.com>

--- Shooting/Shooting/3rd/JobsBaseCustomizeUIKitCore/NSUserDefaults/NSUserDefaults+Manager/NSUserDefaults+Manager.h
+++ Shooting/Shooting/3rd/JobsBaseCustomizeUIKitCore/NSUserDefaults/NSUserDefaults+Manager/NSUserDefaults+Manager.h
@@ -14,6 +14,38 @@
 #import "MJExtension.h"
 #endif
 
+#ifndef SetUserDefaultKeyWithValue
+#define SetUserDefaultKeyWithValue(key,value) [NSUserDefaults.standardUserDefaults setValue:value forKey:key]
+#endif
+
+#ifndef SetUserDefaultKeyWithObject
+#define SetUserDefaultKeyWithObject(key,object) [NSUserDefaults.standardUserDefaults setObject:object forKey:key]
+#endif
+
+#ifndef SetUserBoolKeyWithObject
+#define SetUserBoolKeyWithObject(key,object) [NSUserDefaults.standardUserDefaults setBool:object forKey:key]
+#endif
+
+#ifndef GetUserDefaultValueForKey
+#define GetUserDefaultValueForKey(key) [NSUserDefaults.standardUserDefaults valueForKey:key]
+#endif
+
+#ifndef GetUserDefaultObjForKey
+#define GetUserDefaultObjForKey(key) [NSUserDefaults.standardUserDefaults objectForKey:key]
+#endif
+
+#ifndef GetUserDefaultBoolForKey
+#define GetUserDefaultBoolForKey(key) [NSUserDefaults.standardUserDefaults boolForKey:key]
+#endif
+
+#ifndef DeleUserDefaultWithKey
+#define DeleUserDefaultWithKey(key) [NSUserDefaults.standardUserDefaults removeObjectForKey:key]
+#endif
+
+#ifndef UserDefaultSynchronize
+#define UserDefaultSynchronize NSUserDefaults.standardUserDefaults.synchronize
+#endif
+
 NS_ASSUME_NONNULL_BEGIN
 
 @interface NSUserDefaults (Manager)
